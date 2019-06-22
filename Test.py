3cec98f984dfffe105fca2a600f55e628e3429b2
Dhanush <dhanushdazz@gmail.com>
2019-06-23 01:02:18 +0530

    Added Queue Action.

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 from sinric import _sinricpro
 
-# if __name__ == '__main__':
-ob = _sinricpro.Sinric('7368dc0f-1893-4e4f-bbae-6439ae26b8eb')
-ob.initialize()
+if __name__ == '__main__':
+    ob = _sinricpro.Sinric('Api  Key')
+    ob.initialize()
