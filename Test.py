79b9d26842c98039b8018c7178ef65d7e21795c0
Dhanush <dhanushdazz@gmail.com>
2019-06-26 01:18:15 +0530

    Code Reduced

--- Test.py
+++ Test.py
@@ -1,59 +1,15 @@
-import websockets as ws
+from sinric._sinricpro import Produce, Consume
 import asyncio
-import logging as log
-from time import sleep
-from threading import Thread
 from queue import Queue
-
-
-class Sinric:
-    def __init__(self, apiKey, deviceId):
-        self.apiKey = apiKey
-        self.myQueue = Queue()
-        self.deviceId = deviceId
-
-    async def listen(self):
-        async with ws.connect(
-                'ws://23.95.122.232:3001',
-                extra_headers={'Authorization': self.apiKey, 'deviceids': self.deviceId}) as websocket:
-            greeting = await websocket.recv()
-            self.myQueue.put(greeting)
-
-    def Action(self):
-        while self.myQueue.qsize() > 0:
-            print(self.myQueue.get())
-
-    async def performAction(self):
-        if self.myQueue.qsize() > 0:
-            Thread(target=self.Action).start()
-
-        else:
-            return
-
-    def initialize(self):
-        while True:
-            try:
-                asyncio.get_event_loop().run_until_complete(self.listen())
-                asyncio.get_event_loop().run_until_complete(self.performAction())
-            except Exception as e:
-                print(e)
-
 
 apiKey = 'Api Key'
 deviceId = 'Device Id'
 
 if __name__ == '__main__':
-    obj = Sinric(apiKey, deviceId)
-    obj.initialize()
-
-# from sinric._sinricpro import SinricPro, Consumer
-# import multiprocessing as mp
-#
-# apiKey = '6125c4a0-2d5b-4d0b-a0c6-70774d6c0b69'
-# deviceId = '5d11133035d6f2121c7dccb6'
-# if __name__ == '__main__':
-#     with mp.Pool(3) as pool:
-#         m = mp.Manager()
-#         data_q = m.Queue()
-#         p = pool.map_async(SinricPro, (apiKey, deviceId, data_q,))
-#         c = pool.map_async(Consumer, (data_q,))
+    myq = Queue()
+    while True:
+        try:
+            asyncio.get_event_loop().run_until_complete(Produce(apiKey, deviceId, myq))
+            asyncio.get_event_loop().run_until_complete(Consume(myq))
+        except Exception as e:
+            print(e)
--- sinric/_sinricpro.py
+++ sinric/_sinricpro.py
@@ -1,47 +1,16 @@
-import websocket
-import json
-from queue import Queue
-import time
-import asyncio
+import websockets as ws
 
 
-def SinricPro(apiKey, deviceId, my_q):
-    def on_message(ws, message):
-        # obj = json.loads(message)
-        my_q.put(json.loads(message))
-        # print(message)
-
-    def on_error(ws, error):
-        print(error)
-
-    def on_close(ws):
-        print("### closed ###")
-        time.sleep(2)
-        initiate()
-
-    def on_open(ws):
-        print("### Initiating new websocket connection ###")
-
-    def initiate():
-        websocket.enableTrace(False)
-
-        ws = websocket.WebSocketApp("ws://23.95.122.232:3001",
-                                    header={'Authorization:' + apiKey,
-                                            'deviceids:' + deviceId},
-                                    on_message=on_message,
-                                    on_error=on_error,
-                                    on_close=on_close)
-        ws.on_open = on_open
-
-        ws.run_forever()
-
-    initiate()
+async def Produce(apiKey, deviceId, queue):
+    async with ws.connect(
+            'ws://23.95.122.232:3001',
+            extra_headers={'Authorization': apiKey, 'deviceids': deviceId}) as websocket:
+        greeting = await websocket.recv()
+        queue.put(greeting)
 
 
-def Consumer(my_q):
-        if my_q.qsize() > 0:
-            print('My Consume : ', my_q.get())
-            my_q.task_done()
-        else:
-            print('Queue Empty')
-            return
+async def Consume(queue):
+    if queue.qsize() > 0:
+        print(queue.get())
+    else:
+        return
