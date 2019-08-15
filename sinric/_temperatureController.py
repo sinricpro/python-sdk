29d54d7dbc977cee0efbe5b7ad3951e5f9c826ba
Dhanush <dhanushdazz@gmail.com>
2019-08-15 16:35:29 +0530

    Keep track of temperature

--- sinric/_temperatureController.py
+++ sinric/_temperatureController.py
@@ -3,6 +3,7 @@ from sinric._jsoncommands import JSON_COMMANDS
 
 class TemperatureController:
     def __init__(self, k):
+        self.temperature = 0
         pass
 
     async def targetTemperature(self, jsn, callback):
@@ -10,4 +11,9 @@ class TemperatureController:
                         jsn.get(JSON_COMMANDS.get('VALUE')).get('schedule').get('duration'))
 
     async def adjustTemperature(self, jsn, callback):
-        return callback(jsn.get(JSON_COMMANDS['DEVICEID']), jsn.get(JSON_COMMANDS.get('VALUE')).get('temperature'))
+        self.temperature+=jsn.get(JSON_COMMANDS.get('VALUE')).get('temperature')
+        if self.temperature < 0:
+            self.temperature=0
+        elif self.temperature > 100:
+            self.temperature=100
+        return callback(jsn.get(JSON_COMMANDS['DEVICEID']),self.temperature )
