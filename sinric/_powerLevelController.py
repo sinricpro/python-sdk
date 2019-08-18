a4dc57b9bf6d53a09c2116f335381d1a70f67b61
Dhanush <dhanushdazz@gmail.com>
2019-08-18 16:36:11 +0530

    Store powerLevel and rangeValue

--- sinric/_powerLevelController.py
+++ sinric/_powerLevelController.py
@@ -1,8 +1,20 @@
 from sinric._jsoncommands import JSON_COMMANDS
+import json
+
 
 class PowerLevel:
     def __init__(self, x):
         self.power_level = x
+        with open('localdata.json', 'r') as f:
+            self.data2 = json.load(f)
+            self.power_level = self.data2.get('powerLevel', 0)
+            f.close()
+
+    def dumpData(self, key, val):
+        with open('localdata.json', 'w') as f:
+            self.data2.update({key: val})
+            json.dump(self.data2, f)
+            f.close()
 
     async def setPowerLevel(self, jsn, power_level_callback):
         self.power_level = jsn[JSON_COMMANDS['VALUE']][JSON_COMMANDS['POWERLEVEL']]
@@ -15,5 +27,6 @@ class PowerLevel:
             self.power_level = 100
         elif self.power_level < 0:
             self.power_level = 0
+        self.dumpData('powerLevel', self.power_level)
         return adjust_power_level_cb(jsn[JSON_COMMANDS['DEVICEID']],
                                      self.power_level)
--- sinric/_rangeValueController.py
+++ sinric/_rangeValueController.py
@@ -1,12 +1,29 @@
 from ._jsoncommands import JSON_COMMANDS
-
+import json
 
 class RangeValueController:
     def __init__(self, k):
-        pass
+        self.rangeValue = 0
+        with open('localdata.json','r') as  f:
+            self.rangeDat = json.load(f)
+            self.rangeValue = self.rangeDat.get('rangeValue',0)
+            f.close()
+
+    def dumpData(self,key,val):
+        self.rangeDat.update({key,val})
+        with open('localdata.json','w') as f:
+            json.dump(self.rangeDat,f)
+            f.close()
 
     async def setRangeValue(self, jsn, range_callback):
-        return range_callback(jsn.get(JSON_COMMANDS.get('DEVICEID')), jsn[JSON_COMMANDS['VALUE']]['rangeValue'])
+        self.rangeValue= jsn.get(JSON_COMMANDS['VALUE']).get('rangeValue')
+        return range_callback(jsn.get(JSON_COMMANDS.get('DEVICEID')), self.rangeValue)
 
     async def adjustRangeValue(self, jsn, callback):
-        return callback(jsn.get(JSON_COMMANDS.get('DEVICEID')), jsn[JSON_COMMANDS['VALUE']]['rangeValue'])
+        self.rangeValue += jsn.get(JSON_COMMANDS['VALUE']).get('rangeValue')
+        if self.rangeValue > 100:
+            self.rangeValue = 100
+        elif self.rangeValue < 0:
+            self.rangeValue = 0
+        self.dumpData('rangeValue', self.rangeValue)
+        return callback(jsn.get(JSON_COMMANDS.get('DEVICEID')), self.rangeValue)
