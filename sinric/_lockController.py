6c674b6fde4ef1fff3c2c2765ad09ab4224c0502
Dhanush <dhanushdazz@gmail.com>
2019-08-31 15:00:21 +0530

    Default lock state --> unlocked

--- sinric/_lockController.py
+++ sinric/_lockController.py
@@ -7,4 +7,4 @@ class LockStateController:
         pass
 
     async def setLockState(self, jsn, callback):
-        return callback(jsn.get("deviceId"), jsn.get('value', False).get('state', False))
+        return callback(jsn.get("deviceId"), jsn.get('value', False).get('state', "UNLOCKED"))
