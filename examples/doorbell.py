4374fe960b8017a40d26cd074b724fe2e7cf8320
cojmeister <luqas.c@gmail.com>
2023-12-28 21:16:56 -0500

    Typings on doorbell.py

--- examples/doorbell.py
+++ examples/doorbell.py
@@ -1,9 +1,10 @@
+from typing import Final
 from sinric import SinricPro, SinricProConstants
 import asyncio
 
-APP_KEY = ''
-APP_SECRET = ''
-DOORBELL_ID = ''
+APP_KEY: Final[str] = ''
+APP_SECRET: Final[str] = ''
+DOORBELL_ID: Final[str] = ''
 
 '''
 DON'T FORGET TO TURN ON 'Doorbell Press' IN ALEXA APP
@@ -19,9 +20,9 @@ async def events():
 callbacks = {}
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    client = SinricPro(APP_KEY, [DOORBELL_ID], callbacks, event_callbacks=events,
-                       enable_log=True, restore_states=False, secret_key=APP_SECRET)
+    loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
+    client: SinricPro = SinricPro(APP_KEY, [DOORBELL_ID], callbacks, event_callbacks=events,
+                                  enable_log=True, restore_states=False, secret_key=APP_SECRET)
     loop.run_until_complete(client.connect())
 
     # client.event_handler.raise_event(DOORBELL_ID, SinricProConstants.DOORBELLPRESS)
