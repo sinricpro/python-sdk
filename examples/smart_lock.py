a305bf57fab2519f3d3ea787da42490f4652f1b1
cojmeister <luqas.c@gmail.com>
2023-12-31 06:09:21 -0500

    Typings for smart lock

--- examples/smart_lock.py
+++ examples/smart_lock.py
@@ -1,24 +1,29 @@
+from collections.abc import Callable
+from typing import Final
 from sinric import SinricPro, SinricProConstants
 import asyncio
 
-APP_KEY = ''
-APP_SECRET = ''
-LOCK_ID = ''
+APP_KEY: Final[str] = ''
+APP_SECRET: Final[str] = ''
+LOCK_ID: Final[str] = ''
+
+CallbackFunctions = Callable[[str, str], tuple[bool, str]]
 
 
-def lock_state(device_id, state):
+def lock_state(device_id: str, state: str) -> tuple[bool, str]:
+    # TODO: state should definitely be an enum
     print('device_id: {} lock state: {}'.format(device_id, state))
     return True, state
 
 
-callbacks = {
+callbacks: dict[str, CallbackFunctions] = {
     SinricProConstants.SET_LOCK_STATE: lock_state
 }
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    client = SinricPro(APP_KEY, [LOCK_ID], callbacks, enable_log=False,
-                       restore_states=False, secret_key=APP_SECRET)
+    loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
+    client: SinricPro = SinricPro(APP_KEY, [LOCK_ID], callbacks, enable_log=False,
+                                  restore_states=False, secret_key=APP_SECRET)
     loop.run_until_complete(client.connect())
 
 
--- sinric/_lock_controller.py
+++ sinric/_lock_controller.py
@@ -5,11 +5,14 @@
  *  This file is part of the Sinric Pro (https://github.com/sinricpro/)
 """
 
+from collections.abc import Callable
+from typing import Any
 from ._sinricpro_constants import SinricProConstants
+
 
 class LockStateController:
     def __init__(self):
-        self.lock_state = ''
+        self.lock_state: str = ''
 
-    async def set_lock_state(self, jsn, callback):
+    async def set_lock_state(self, jsn: Any, callback: Callable[[str, str], tuple[bool, str]]) -> tuple[bool, str]:
         return callback(jsn.get(SinricProConstants.DEVICEID), jsn.get("payload").get('value', False).get(SinricProConstants.STATE, "unlock"))
