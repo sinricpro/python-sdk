f0c7e4a3f5e72e4b79aa742a6b4f9b02f3d41c66
cojmeister <luqas.c@gmail.com>
2024-01-13 18:45:05 -0300

    Typings for leaky bucket

--- sinric/_leaky_bucket.py
+++ sinric/_leaky_bucket.py
@@ -1,28 +1,31 @@
 import time
+from typing import Final
+
 
 class LeakyBucket:
-    def __init__(self, bucket_size, drop_in_time, drop_out_time):
-        self.bucket_size = bucket_size
-        self.drop_in_time = drop_in_time
-        self.drop_out_time = drop_out_time
-        self.drop_in_bucket = 0
-        self.last_drop = 0
+    def __init__(self, bucket_size: int, drop_in_time: int, drop_out_time: int):
+        self.bucket_size: Final[int] = bucket_size
+        self.drop_in_time: int = drop_in_time
+        self.drop_out_time: int = drop_out_time
+        self.drop_in_bucket: int = 0
+        self.last_drop: int = 0
 
-    def millis(self):
+    def millis(self) -> int:
         return int(round(time.time() * 1000))
 
-    def leak_drops(self):
-        actual_millis = self.millis()
-        drops_to_leak = round((actual_millis - self.last_drop) / self.drop_out_time)
+    def leak_drops(self) -> None:
+        actual_millis: int = self.millis()
+        drops_to_leak: int = round(
+            (actual_millis - self.last_drop) / self.drop_out_time)
         if drops_to_leak > 0:
             if self.drop_in_bucket <= drops_to_leak:
                 self.drop_in_bucket = 0
             else:
                 self.drop_in_bucket = self.drop_in_bucket - drops_to_leak
 
-    def add_drop(self):
+    def add_drop(self) -> bool:
         self.leak_drops()
-        actual_millis = self.millis()
+        actual_millis: int = self.millis()
         if (self.drop_in_bucket < self.bucket_size) and (
                 (actual_millis - self.last_drop) > (self.drop_in_bucket + self.drop_in_time)):
             self.drop_in_bucket = self.drop_in_bucket + 1
